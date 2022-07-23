8b72509a3ec4418e8ac617b87d2a619922c50610
smartdev328 <solid.dev3028@gmail.com>
2022-07-23 08:52:56 -0700

    new site design
    new logo
    pricing page

--- packages/site/tailwind.config.cjs
+++ packages/site/tailwind.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Grotesk', 'Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
